46803bcfe06a379197d6fc5599d1d14454ea5221
zombieS12 <93500458+zombieS12@users.noreply.github.com>
2022-07-19 16:30:52 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const exp = require("express");
+const fs = require('fs');
+const bodyParser = require("body-parser");
+// const jwt = require('jsonwebtoken');
+var cors = require('cors')
+const app = exp();
+const port = 3000;
+const http = require('http');
+const server = http.createServer(app);
+ 
+// const PRIVATE_KEY = fs.readFileSync('private-key.txt');
+app.use(bodyParser.json()); 
+// app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+var mysql = require('mysql');
+var db = mysql.createConnection({
+     host: 'localhost',
+     user: 'root', 
+     password: '', 
+     database: 'ksdn',
+
+}); 
+db.connect(() => console.log('Da ket noi database !'));
+module.exports = db; 
+app.get("/", (req, res) => {
+     res.send("Đây là trang chủ")
+});
+
+// io.on('connection', (socket) => {
+//      socket.on('phone', (sdt) => {
+//           io.emit('user',sdt)
+
+//           console.log('phone: ' + sdt);
+//      });
+// });
+
+
+app.get('/api/loai', (req, res) => {
+     // const email = req.params.id;
+     // const password = req.body.password;
+     let sql = 'SELECT * FROM loai';
+     db.query(sql, (err, rows) => { 
+          res.status(200).json(rows)
+          // console.log(err)
+
+     })
+})
+app.get('/api/sanpham', (req, res) => {
+     // const email = req.params.id;
+     // const password = req.body.password;
+     let sql = 'SELECT * FROM san_pham';
+     db.query(sql, (err, rows) => { 
+          res.json(rows)
+          // console.log(err)
+
+     })
+})
+
+app.post('/api/sanpham', (req, res) => {
+     let tensp = req.body.tensp;
+     let gia_nhap = req.body.giaNhap;
+     let gia_ban = req.body.giaBan;
+     let images = req.body.images;
+     let mota = req.body.mota;
+     let maLoai = req.body.maLoai;
+     let date = req.body.date;
+
+     let data ={
+          ten_san_pham:tensp,
+          gia_nhap:gia_nhap,
+          gia_ban:gia_ban,
+          images:images,
+          mo_ta:mota,
+          ma_loai:maLoai,
+          ngay_them:date
+     };  
+     // const password = req.body.password;
+     let sql = 'INSERT INTO san_pham SET ?';
+     db.query(sql,data, function(err, d) { 
+          res.json(d)
+          console.log(err)
+     }  ); 
+})
+
+app.post('/api/loai', (req, res) => {
+     let tenloai = req.body.tenLoai;
+     let user_info ={tenLoai:tenloai};  
+     // const password = req.body.password;
+     let sql = 'INSERT INTO loai SET ?';
+     db.query(sql,user_info, function(err, d) { 
+          res.json(d)
+          console.log(err)
+     }  ); 
+})
+
+
+
+
+app.listen(port, () =>{
+     console.log(`Ung dung dang chay voi port ${port}`);
+});
